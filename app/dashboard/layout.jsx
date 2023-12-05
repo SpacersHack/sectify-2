5c6233c4496aad26a18edb10bcd743ae837e75ea
haybuka <paschal.nduleo@gmail.com>
2023-12-05 23:06:48 +0100

    dashboard Ui

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google';
 import cls from 'classname';
 import NavElements from './navigation';
+import DashboardHeader from './header';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -8,8 +9,9 @@ export default function RootLayout({ children }) {
   return (
     <main className="w-screen h-screen overflow-hidden bg-[#F5F5F5] flex items-start">
       <NavElements />
-      <section className={cls(inter.className, 'overflow-y-scroll p-6')}>
-        {children}
+      <section className={cls(inter.className, 'w-full h-full')}>
+        <DashboardHeader />
+        <aside className="overflow-y-scroll p-6">{children}</aside>
       </section>
     </main>
   );
--- app/dashboard/navigation/index.jsx
+++ app/dashboard/navigation/index.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import cls from 'classname';
 import styles from './nav.module.css';
 import { usePathname, useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 const navigation = [
   {
@@ -29,14 +30,15 @@ const navigation = [
 
 const NavElements = () => {
   const pathname = usePathname();
-  console.log(pathname);
+
   return (
-    <section className="w-1/6 bg-white h-full">
+    <section className="w-3/6 md:w-1/6 bg-white h-full">
       <h3 className="my-3 text-xl font-bold text-[#009947] pl-10">SECTIFY</h3>
       <aside className="py-6 h-full flex flex-col justify-between">
         <ul>
           {navigation.map((nav, id) => (
-            <li
+            <Link
+              href={nav.route}
               key={id}
               className={cls(
                 styles.navItem,
@@ -44,7 +46,7 @@ const NavElements = () => {
               )}
             >
               {nav.name}
-            </li>
+            </Link>
           ))}
         </ul>
         <p className="pl-10 my-3 py-3 capitalize mb-10 text-sectify-red">
